1921374a2ab554df2ea3b25167eb61ce81c11d74
mine <mine@localhost.localdomain>
2022-08-23 15:35:53 +0300

    added variable configuration and logs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, InternalServerErrorException, UnauthorizedException, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+  ConflictException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth.credential.dto';
@@ -12,50 +17,51 @@ export class AuthService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
-    private jwtService: JwtService
-  ) { }
-
+    private jwtService: JwtService,
+  ) {}
 
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { email, username, password } = authCredentialsDto;
 
-    const salt = await bcrypt.genSalt()//generate salt
-    const hashedPassword = await bcrypt.hash(password, salt)//hashed our password
+    const salt = await bcrypt.genSalt(); //generate salt
+    const hashedPassword = await bcrypt.hash(password, salt); //hashed our password
 
     const user = this.userRepository.create({
       email,
       username,
-      password: hashedPassword
-    })
+      password: hashedPassword,
+    });
 
     try {
-      await this.userRepository.save(user)
+      await this.userRepository.save(user);
     } catch (error) {
+      // this.logger.error(`Failed to create user ${user.email}`);
+
       if (error.code === '23505') {
-        throw new ConflictException('Username or Email already exists')
+        throw new ConflictException('Username or Email already exists');
       }
-      throw new InternalServerErrorException()
+      throw new InternalServerErrorException();
     }
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { email, username, password } = authCredentialsDto;
 
-    const user = await this.userRepository.findOne({ where: { email, username } })
+    const user = await this.userRepository.findOne({
+      where: { email, username },
+    });
 
     // console.log(user)
-    if (user && (bcrypt.compare(password, user.password))) {
+    if (user && bcrypt.compare(password, user.password)) {
       //return 'Success'
-      const payload: JwtPayload = { username, email }
-      const accessToken: string = await this.jwtService.sign(payload)
+      const payload: JwtPayload = { username, email };
+      const accessToken: string = await this.jwtService.sign(payload);
 
-      return { accessToken }
+      return { accessToken };
     } else {
-      throw new UnauthorizedException('Plase check your credentials')
+      throw new UnauthorizedException('Plase check your credentials');
     }
-
-
   }
-
-
 }
