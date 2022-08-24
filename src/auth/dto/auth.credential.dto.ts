a652f2e4b0811c6887356b10681e2b365df8357c
mine <mine@localhost.localdomain>
2022-08-24 22:06:19 +0300

    validation credentials

--- src/auth/dto/auth.credential.dto.ts
+++ src/auth/dto/auth.credential.dto.ts
@@ -1,5 +1,27 @@
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsEmail()
+  @IsString()
   email: string;
+
+  @MinLength(4)
+  @MaxLength(20)
+  @IsString()
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must have at least one lowercase and uppercase, a number or special characters.',
+  })
   password: string;
 }
